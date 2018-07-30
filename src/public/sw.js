63fc1c63bc70eacc1e5c9b1de872607b2745d18e
OldSchoolBoo <booelle.commissions@gmail.com>
2018-07-29 23:30:29 -0700

    refactor: SW caches all stylesheets that are limited by media queries

--- src/public/sw.js
+++ src/public/sw.js
@@ -29,7 +29,12 @@ self.addEventListener('install', (event) =>{
         'js/restaurant_info.js',
         'js/service_worker.js',
         'js/idb.js',
-        'css/styles.css'
+        'css/styles.css',
+        'css/styles-798.css',
+        'css/styles-615.css',
+        'css/styles-541.css',
+        'css/styles-498.css',
+        'css/styles-323.css'
       ]);
     })
   );
